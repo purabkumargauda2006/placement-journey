06b102159bf6240ea60f34e5ea72afad4bba7bfb
Purab <purabkumargauda2006@gmail.com>
2026-07-01 14:52:19 +0530

    OOP basics - Student class with constructor and methods

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+                Student s1 = new Student("Purab", 20, 7.9f, "GIET University");
+                Student s2 = new Student("Rahul", 21, 8.2f, "NIT Rourkela");
+
+                s1.introduce();
+                s2.introduce();
+            }
+        }
+
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    String name;
+    int age;
+    float gpa;
+    String college;
+    public Student(String name, int age, float gpa, String college) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        this.college = college;
+    }
+     public void introduce(){
+         System.out.println("Hi my name is" +name);
+                 System.out.println("my cgpa is" +gpa);
+
+        }
+
+        }
+
+
+
