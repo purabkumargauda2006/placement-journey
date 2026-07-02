ccfc7dc2125f7110006dce698f7fbcc0674ef4e3
Purab <purabkumargauda2006@gmail.com>
2026-07-02 09:47:03 +0530

    OOP encapsulation - private fields, getters and setters

--- src/Main2.java
+++ src/Main2.java
@@ -3,6 +3,10 @@ public class Main2 {
 
                 Student s1 = new Student("Purab", 20, 7.9f, "GIET University");
                 Student s2 = new Student("Rahul", 21, 8.2f, "NIT Rourkela");
+        System.out.println(s1.getAge());
+        s1.setAge(-5);
+        s1.setAge(25);
+        System.out.println(s1.getAge());
 
                 s1.introduce();
                 s2.introduce();
--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,44 @@
 public class Student {
-    String name;
-    int age;
-    float gpa;
-    String college;
+    private String name;
+    private int age;
+    private float gpa;
+    private String college;
     public Student(String name, int age, float gpa, String college) {
         this.name = name;
         this.age = age;
         this.gpa = gpa;
         this.college = college;
     }
-     public void introduce(){
-         System.out.println("Hi my name is" +name);
-                 System.out.println("my cgpa is" +gpa);
+     public void introduce() {
+         System.out.println("Hi my name is" + name);
+         System.out.println("my cgpa is" + gpa);
+     }
+         public int getAge() {
+             return age;
+         }
 
-        }
+         public void setAge(int age) {
+             if (age > 0 && age < 100) {
+                 this.age = age;
+             } else {
+                 System.out.println("Invalid age");
+             }
+         }
 
-        }
+         public String getName() {
+             return name;
+         }
+
+         public void setName(String name) {
+             this.name = name;
+         }
+
+
+         }
+
+
+
+
 
 
 
